29799b0a9443d767bdc0c1633399e62f1a1ecceb
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2023-05-19 12:41:51 +0100

    require imports

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -1,5 +1,5 @@
 import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes'
-import LiveSnippetNavbarItem from '@site/src/components/LiveSnippets/NavbarButton'
+import LiveSnippetNavbarItem from require('@site/src/components/LiveSnippets/NavbarButton')
 
 export default {
   ...ComponentTypes,
